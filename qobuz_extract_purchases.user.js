caafc0b4509124a1470d148a5fce342cef8d8fd4
Maxr1998 <max.rumpf1998@gmail.com>
2024-01-26 03:49:52 +0100

    :sparkles: Qobuz extract purchases: support tracks

--- qobuz_extract_purchases.user.js
+++ qobuz_extract_purchases.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/Maxr1998/userscripts
 // @supportURL   https://github.com/Maxr1998/userscripts/issues
 // @downloadURL  https://github.com/Maxr1998/userscripts/raw/master/qobuz_extract_purchases.user.js
-// @version      0.1
+// @version      0.1.1
 // @description  Extracts simple metadata about purchased albums from your Qobuz account.
 // @author       Maxr1998
 // @match        https://www.qobuz.com/profile/downloads*
@@ -15,9 +15,10 @@
 
     const purchasePageHeaderQuery = '.account-purchases__header';
     const purchaseRowQuery = '.account-purchases__table-row';
-    const albumTitleQueryNoLink = '.account-purchases__album-title';
+    const albumTitleQueryNoLink = 'span.account-purchases__album-title';
     const albumTitleQuery = `${albumTitleQueryNoLink} > a`;
-    const artistQuery = '.account-purchases__album-artist > a';
+    const trackFavoritesQuery = 'a.account-purchases__track--favorites';
+    const artistQuery = 'span.account-purchases__album-artist > a';
     const paginatorQuery = '.account-purchases__pagination.store-paginator';
     const paginatorNextButtonQuery = 'a.store-paginator__button[rel="next"]';
 
@@ -45,34 +46,46 @@
         console.log('Starting purchase extraction…');
         $extractPurchasesButton.hide();
 
-        const results = [];
-        let hasNext = true;
-        for (let page = 1; hasNext; page++) {
-            const $pageContent = await getDownloadsPage(page);
-            const purchases = extractPurchases($pageContent);
-            results.push(...purchases);
-            console.log(`Extracted page ${page}.`)
-
-            const $paginator = $(paginatorQuery, $pageContent);
-            hasNext = $(paginatorNextButtonQuery, $paginator).length > 0
-        }
+        const albums = await getDownloads(
+            (page) => `https://www.qobuz.com/profile/downloads?page=${page}`,
+            processAlbumPurchaseRow
+        );
+        const tracks = await getDownloads(
+            (page) => `https://www.qobuz.com/profile/downloads/track?page=${page}`,
+            processTrackPurchaseRow
+        );
         console.log('Finished extracting purchases.');
         $extractPurchasesButton.show();
 
+        const results = {albums, tracks};
         const resultJson = JSON.stringify(results, null, 2);
         const $modalContent = $('.modal__content p code', $resultModal);
         const $modalCopyButton = $('#copy-purchases', $resultModal);
 
         $modalContent[0].innerText = resultJson;
-        $modalCopyButton.click(function () {
+        $modalCopyButton.click(() => {
             navigator.clipboard.writeText(resultJson);
         });
         $resultModal.modal();
     }
 
-    function getDownloadsPage(page) {
+    async function getDownloads(urlProvider, rowExtractor) {
+        const results = [];
+        let hasNext = true;
+        for (let page = 1; hasNext; page++) {
+            const $pageContent = await loadPageContent(urlProvider(page));
+            const purchases = $(purchaseRowQuery, $pageContent).map((idx, $el) => rowExtractor($el)).toArray();
+            results.push(...purchases);
+
+            const $paginator = $(paginatorQuery, $pageContent);
+            hasNext = $(paginatorNextButtonQuery, $paginator).length > 0
+        }
+        return results;
+    }
+
+    function loadPageContent(url) {
         return new Promise((resolve, reject) => {
-            $('<div>').load(`https://www.qobuz.com/profile/downloads?page=${page}`, function (_, textStatus, __) {
+            $('<div>').load(url, function (_, textStatus, __) {
                 if (textStatus !== 'error') {
                     resolve($(this));
                 } else {
@@ -82,17 +95,29 @@
         });
     }
 
-    function extractPurchases($downloadsPageContent) {
-        return $(purchaseRowQuery, $downloadsPageContent).map(function (idx, $el) {
-            const album = $(albumTitleQuery, $el).attr('title') ??
-                $(albumTitleQueryNoLink, $el).text().trim();
-            const artist = $(artistQuery, $el).attr('title');
-            let url = $(albumTitleQuery, $el).attr('href');
-            if (url && url.startsWith('/')) {
-                url = `https://www.qobuz.com${url}`
-            }
-            return {album, artist, url};
-        }).toArray();
+    function processAlbumPurchaseRow($purchaseRow) {
+        const album = $(albumTitleQuery, $purchaseRow).attr('title') ??
+            $(albumTitleQueryNoLink, $purchaseRow).text().trim();
+        const artist = $(artistQuery, $purchaseRow).attr('title');
+        let url = $(albumTitleQuery, $purchaseRow).attr('href');
+        if (url && url.startsWith('/')) {
+            url = `https://www.qobuz.com${url}`
+        }
+        return {album, artist, url};
+    }
+
+    function processTrackPurchaseRow($purchaseRow) {
+        // Qobuz uses the album title row for the track title…
+        const track = $(albumTitleQuery, $purchaseRow).attr('title') ??
+            $(albumTitleQueryNoLink, $purchaseRow).text().trim();
+        // The "track" contains the album title
+        const album = $(trackFavoritesQuery, $purchaseRow).text().trim();
+        const artist = $(artistQuery, $purchaseRow).attr('title');
+        let url = $(trackFavoritesQuery, $purchaseRow).attr('href');
+        if (url && url.startsWith('/')) {
+            url = `https://www.qobuz.com${url}`
+        }
+        return {track, album, artist, url};
     }
 
     function insertPageElements() {
